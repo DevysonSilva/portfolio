ab4e13e33f65f3620234164a06d8683cae01942f
Devyson Silva <devysonssilva@gmail.com>
2025-05-26 23:21:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+  
+  document.addEventListener('DOMContentLoaded', () => {
+  // MENU
+  const menuToggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('.nav');
+
+  menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('open');
+  });
+
+  // FORMULÁRIO
+  const form = document.getElementById("meu-formulario");
+  const message = document.getElementById("form-message");
+
+  emailjs.init("lLiLdv2wKNtAHuMoK"); // Public Key
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    emailjs.sendForm(
+      "service_2xoltvh",
+      "template_7c70kip",
+      form
+    ).then(
+      function () {
+        message.textContent = "Obrigado! Responderei o mais breve possível.";
+        message.classList.add("show");
+        form.reset();
+      },
+      function (error) {
+        alert("Ocorreu um erro ao enviar a mensagem.");
+        console.log("Erro:", error);
+      }
+    );
+
+    // Oculta a mensagem após 5 segundos
+    setTimeout(() => {
+      message.classList.remove("show");
+      message.textContent = "";
+    }, 5000);
+  });
+});
